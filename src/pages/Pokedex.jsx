6faa74a5408556945ea78afdc9e987a84933a380
Cl Sayart <clsayart@gmail.com>
2021-05-24 13:04:01 +0200

    attempt ternaire filtres

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -10,12 +10,12 @@ import Filters from "../components/Filters";
 
 class Pokedex extends React.Component {
   state = {
-    pokemons:[],
-    search:'',
+    pokemons: [],
+    search: "",
     // electric: false,
-    types:[],
-    typesChecked: []
-  }
+    types: [],
+    typesChecked: [],
+  };
 
   componentDidMount() {
     apiHandler
@@ -28,7 +28,7 @@ class Pokedex extends React.Component {
         console.log(error);
       });
 
-      apiHandler
+    apiHandler
       .getTypes()
       .then((response) => {
         // console.log("TYPES",response)
@@ -38,80 +38,78 @@ class Pokedex extends React.Component {
         console.log(error);
       });
   }
-  
 
   handleChange = (event) => {
-    this.setState({electric: !this.state.electric});
-  }
+    this.setState({ electric: !this.state.electric });
+  };
 
   handleSearch = (valueFromSearch) => {
     this.setState({ search: valueFromSearch });
   };
 
   handleChangeInput = (event) => {
-    const target = event.target
-    if(target.checked) {
-      console.log("SALUT BÉBÉ")
+    const target = event.target;
+    if (target.checked) {
+      console.log("SALUT BÉBÉ");
       const array = this.state.typesChecked;
       array.push(target.name);
-      console.log(array)
-      this.setState({typesChecked: array})
-    }
-    else {
-      console.log("BYE LE GROS")
+      console.log(array);
+      this.setState({ typesChecked: array });
+    } else {
+      console.log("BYE LE GROS");
       const array = this.state.typesChecked;
       const toRemove = array.indexOf(target.name);
       array.splice(toRemove, 1);
-      console.log(array)
-      this.setState({typesChecked: array})
+      console.log(array);
+      this.setState({ typesChecked: array });
     }
-  }
-
+  };
 
   render() {
-const  { search } = this.state;
-const { pokemons } = this.state;
-const { electric } = this.state;
-const  { types } = this.state;
-const { typesChecked } = this.state;
-// console.log(electric);
-if (this.state.pokemons === []) {
-  return <div>Loading...</div>;
-} 
+    const { search } = this.state;
+    const { pokemons } = this.state;
+    const { electric } = this.state;
+    const { types } = this.state;
+    const { typesChecked } = this.state;
+    // console.log(electric);
+    if (this.state.pokemons === []) {
+      return <div>Loading...</div>;
+    }
     return (
-    <div>
+      <div>
         <NavMain />
-        <Filters types={types} handleChangeInput={this.handleChangeInput}/>
-        <FilterSearchBar search={search} electric={electric} handleChange={this.handleChange} handleSearchFn={this.handleSearch}/>
+        <Filters types={types} handleChangeInput={this.handleChangeInput} />
+        <FilterSearchBar
+          search={search}
+          electric={electric}
+          handleChange={this.handleChange}
+          handleSearchFn={this.handleSearch}
+        />
         {pokemons
-        .filter((item) => {
-          item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-
-          let typeNames =[]
-          for (let i=0;i<item.types.length;i++){
-           typeNames.push(item.types[i].type.name)
-           console.log(typeNames)
+          .filter((item) => 
+          item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) && item.types.length === 2 ? item.types[0].type.name.includes(typesChecked) || item.types[1].type.name.includes(typesChecked) : item.types[0].type.name.includes(typesChecked))
+          //item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) && item.types[0].type.name.includes(typesChecked))
+          //mettre un ternaire pour savoir si un ou deux types, et selon ça mettre type 0 et type 1
+          .map((item, index) => <PokemonsList electric={electric} key={index} pokemons={item}/>)
           }
         
-          typeNames.includes(typesChecked)
-          
-        }
-        
-        )
-        .map((item, index) => <PokemonsList electric={electric} key={index} pokemons={item}/>)
-        }
+{/* 
+          .filter((item) => {
+             let typeNames = [];
+              for (let i = 0; i < item.types.length; i++) {
+              typeNames.push(item.types[i].type.name);
+               console.log("TYPENAMES", typeNames);
+              }
+             
+            
+            return typeNames.includes(typesChecked);
+            
+            item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
+            
+           }) */}
 
-{/* {pokemons
-        .filter((item) => 
-        item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) && item.types[0].type.name.includes(typesChecked))
-        .map((item, index) => <PokemonsList electric={electric} key={index} pokemons={item}/>)
-        } */}
-        
-        <Route exact path={"/pokedex/:id"} component={PokemonDetail}/>
-        
-        
-
-    </div>
+        <Route exact path={"/pokedex/:id"} component={PokemonDetail} />
+      </div>
     );
   }
 }
