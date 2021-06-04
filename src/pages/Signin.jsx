09c8cbf1714b936d1254ae9a6546462d761d6396
Cl Sayart <clsayart@gmail.com>
2021-06-04 11:43:04 +0200

    css forms signup/signin

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import FormSignin from "../components/Forms/FormSignin";
+import NavMain from "../components/NavMain";
+import Footer from "../components/Footer";
 
 const Signin = (props) => {
-  return <FormSignin />;
+  return (
+    <div>
+      <NavMain />
+      <FormSignin />
+      <Footer />
+    </div>
+  );
 };
 
 export default Signin;
